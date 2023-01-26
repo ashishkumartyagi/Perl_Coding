eb1a688fe9f8e3a82740f9d3069a9d7e80395384
supercoolashish <77525184+ashishtyag@users.noreply.github.com>
2023-01-26 15:07:08 +0530

    how to find a file in system

---
+++ findfile.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+
+sub main {
+if (-f '/Users/ashishkumartyagi/Desktop/CAD/ptel-cad1-02') {
+    print "File found\n";
+ }
+ else {
+    print "file not found";
+
+ }
+}
+main();
